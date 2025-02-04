59000a5592e61b0d1c355f85b28a599c8431d897
jipenalver <159101935+centmarde@users.noreply.github.com>
2025-02-05 00:58:24 +0800

    fix: vercel.json

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,6 +1,68 @@
 <template>
   <v-card class="mx-auto pa-10" max-width="600" elevation="8">
-     
+      <v-icon
+        class="close-icon"
+        @click="emit('close-dialog')"
+        right
+      >
+        mdi-close
+      </v-icon>
+      <v-form ref="refVForm" @submit.prevent="onFormSubmit">
+  <v-row dense>
+    <v-col cols="12">
+      <v-text-field
+        v-model="formData.email"
+        label="Email"
+        prepend-inner-icon="mdi-email-outline"
+        :rules="[requiredValidator, emailValidator]"
+      ></v-text-field>
+    </v-col>
+
+    <v-col cols="12">
+      <v-text-field
+        v-model="formData.password"
+        prepend-inner-icon="mdi-lock-outline"
+        label="Password"
+        :type="isPasswordVisible ? 'text' : 'password'"
+        :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+        @click:append-inner="isPasswordVisible = !isPasswordVisible"
+        :rules="[requiredValidator, passwordValidator]"
+      ></v-text-field>
+    </v-col>
+
+    <v-col cols="12">
+      <v-text-field
+        v-model="formData.password_confirmation"
+        label="Password Confirmation"
+        :type="isPasswordConfirmVisible ? 'text' : 'password'"
+        :append-inner-icon="isPasswordConfirmVisible ? 'mdi-eye-off' : 'mdi-eye'"
+        @click:append-inner="isPasswordConfirmVisible = !isPasswordConfirmVisible"
+        :rules="[
+          requiredValidator,
+          confirmedValidator(formData.password_confirmation, formData.password)
+        ]"
+      ></v-text-field>
+    </v-col>
+    <v-col cols="12">
+      <v-radio-group v-model="formData.userType" :rules="[requiredValidator]" row>
+        <v-radio label="Teacher" value="teacher"></v-radio>
+        <v-radio label="Admin" value="admin"></v-radio>
+      </v-radio-group>
+    </v-col>
+  </v-row>
+
+  <v-btn
+    class="mt-2"
+    type="submit"
+     color="#00A4E5"
+    prepend-icon="mdi-account-plus"
+    :disabled="formAction.formProcess"
+    :loading="formAction.formProcess"
+    block
+  >
+    Register
+  </v-btn>
+</v-form>
   </v-card>
 </template>
 
