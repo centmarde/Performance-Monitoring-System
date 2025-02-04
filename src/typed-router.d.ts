b2b03adad1cab8bb38189c0e694dea462c869f10
ccqg <gabaleschrist1@gmail.com>
2025-02-05 02:00:42 +0800

    fix: delete unnecessary files

--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -23,7 +23,5 @@ declare module 'vue-router/auto-routes' {
     '/Home': RouteRecordInfo<'/Home', '/Home', Record<never, never>, Record<never, never>>,
     '/NotFound': RouteRecordInfo<'/NotFound', '/NotFound', Record<never, never>, Record<never, never>>,
     '/Profiles': RouteRecordInfo<'/Profiles', '/Profiles', Record<never, never>, Record<never, never>>,
-    '/sample': RouteRecordInfo<'/sample', '/sample', Record<never, never>, Record<never, never>>,
-    '/sample1': RouteRecordInfo<'/sample1', '/sample1', Record<never, never>, Record<never, never>>,
   }
 }
