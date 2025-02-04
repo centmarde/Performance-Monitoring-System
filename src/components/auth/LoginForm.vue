dc00e56e6234bc700086bc40966843359c2c26cf
ccqg <gabaleschrist1@gmail.com>
2025-02-04 23:43:37 +0800

    Another fix of log-in & register

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,38 +1,19 @@
 <template>
-  <v-card class="pa-13" :class="themeClass" elevation="8">
-    <!-- Image at the Top -->
-    <v-img
-      class="mx-auto"
-      src="/images/school-logo.png"
-      :width="mobile ? '60vw' : '150px'"
-      :height="mobile ? '60vw' : '150px'"
-      cover
-      style="border-radius: 50%; object-fit: cover; margin-bottom: 20px"
-    />
-
-    <h2
-      class="text-h4 font-weight-bold text-light-green-darken-4 d-flex align-center justify-center"
-    >
-      Welcome
-    </h2>
-    <p
-      class="text-body-2 text-grey-darken-1 d-flex align-center justify-center"
-    >
-      Login to your account to continue
-    </p>
-
-    <v-form ref="refVForm" @submit.prevent="onFormSubmit">
-      <v-row dense>
-        <v-col cols="12">
+  <v-col cols="12" md="6">
+    <v-card-text class="mt-12">
+      <h4 class="text-center">Login to Your Account</h4>
+      <h6 class="text-center grey--text mt-2">
+        Log in to your account so you can continue building
+        <br />and editing your onboarding flows
+      </h6>
+      <v-row align="center" justify="center mt-3">
+        <v-col cols="12" sm="8">
           <v-text-field
             v-model="loginEmail"
             label="Email"
             prepend-inner-icon="mdi-email-outline"
             :rules="[requiredValidator, emailValidator]"
           ></v-text-field>
-        </v-col>
-
-        <v-col cols="12">
           <v-text-field
             v-model="loginPassword"
             prepend-inner-icon="mdi-lock-outline"
@@ -42,38 +23,48 @@
             @click:append-inner="isPasswordVisible = !isPasswordVisible"
             :rules="[requiredValidator]"
           ></v-text-field>
+
+          <v-row>
+            <v-col cols="12" sm="7">
+              <v-checkbox
+                label="Remember Me"
+                class="mt-n1"
+                color="blue"
+              ></v-checkbox>
+            </v-col>
+            <v-col cols="12" sm="5">
+              <span class="caption blue--text">Forgot password</span>
+            </v-col>
+          </v-row>
+          <v-btn
+            class="mt-2"
+            type="submit"
+            color="#00A4E5"
+            prepend-icon="mdi-login"
+            :disabled="formAction.formProcess"
+            :loading="formAction.formProcess"
+            block
+          >
+            Login
+          </v-btn>
+
+          <h5 class="text-center grey--text mt-4 mb-3">Or Log in using</h5>
+          <div class="d-flex justify-space-between align-center mx-10 mb-16">
+            <!-- Replaced FontAwesome icons with Vuetify icons -->
+            <v-btn depressed outlined color="grey">
+              <v-icon color="red">mdi-google</v-icon>
+            </v-btn>
+            <v-btn depressed outlined color="grey">
+              <v-icon color="blue">mdi-facebook</v-icon>
+            </v-btn>
+            <v-btn depressed outlined color="grey">
+              <v-icon color="light-blue lighten-3">mdi-twitter</v-icon>
+            </v-btn>
+          </div>
         </v-col>
       </v-row>
-
-      <v-btn
-        class="mt-2 px-10"
-        type="submit"
-        color="light-green-darken-4"
-        prepend-icon="mdi-login"
-        :disabled="formAction.formProcess"
-        :loading="formAction.formProcess"
-        block
-        dark
-      >
-        Login
-      </v-btn>
-
-      <!-- "Don't have an account? Register" on the same line -->
-      <p
-        class="text-body-2 text-grey-darken-1 mt-3 d-flex align-center justify-center"
-      >
-        Don't have an account?
-        <a
-          class="text-light-green-darken-4 font-weight-medium ml-1"
-          href="#"
-          :class="{ 'v-btn--disabled': formAction.formProcess }"
-          @click.prevent="$emit('open-register-dialog')"
-        >
-          Register
-        </a>
-      </p>
-    </v-form>
-  </v-card>
+    </v-card-text>
+  </v-col>
 </template>
 
 <script lang="ts" setup>
