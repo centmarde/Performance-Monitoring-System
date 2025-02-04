82695529dbaeedf6ef65bd032c97a45e7098a2a9
jipenalver <159101935+centmarde@users.noreply.github.com>
2025-02-04 20:58:21 +0800

    feat: start in user auth disable router if in development.

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,69 +1,69 @@
 <template>
-    <v-card class="mx-auto pa-10" max-width="600" elevation="8">
-        <v-icon
-          class="close-icon"
-          @click="emit('close-dialog')"
-          right
-        >
-          mdi-close
-        </v-icon>
-        <v-form ref="refVForm" @submit.prevent="onFormSubmit">
-    <v-row dense>
-      <v-col cols="12">
-        <v-text-field
-          v-model="formData.email"
-          label="Email"
-          prepend-inner-icon="mdi-email-outline"
-          :rules="[requiredValidator, emailValidator]"
-        ></v-text-field>
-      </v-col>
+  <v-card class="mx-auto pa-10" max-width="600" elevation="8">
+      <v-icon
+        class="close-icon"
+        @click="emit('close-dialog')"
+        right
+      >
+        mdi-close
+      </v-icon>
+      <v-form ref="refVForm" @submit.prevent="onFormSubmit">
+  <v-row dense>
+    <v-col cols="12">
+      <v-text-field
+        v-model="formData.email"
+        label="Email"
+        prepend-inner-icon="mdi-email-outline"
+        :rules="[requiredValidator, emailValidator]"
+      ></v-text-field>
+    </v-col>
 
-      <v-col cols="12">
-        <v-text-field
-          v-model="formData.password"
-          prepend-inner-icon="mdi-lock-outline"
-          label="Password"
-          :type="isPasswordVisible ? 'text' : 'password'"
-          :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
-          @click:append-inner="isPasswordVisible = !isPasswordVisible"
-          :rules="[requiredValidator, passwordValidator]"
-        ></v-text-field>
-      </v-col>
+    <v-col cols="12">
+      <v-text-field
+        v-model="formData.password"
+        prepend-inner-icon="mdi-lock-outline"
+        label="Password"
+        :type="isPasswordVisible ? 'text' : 'password'"
+        :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+        @click:append-inner="isPasswordVisible = !isPasswordVisible"
+        :rules="[requiredValidator, passwordValidator]"
+      ></v-text-field>
+    </v-col>
 
-      <v-col cols="12">
-        <v-text-field
-          v-model="formData.password_confirmation"
-          label="Password Confirmation"
-          :type="isPasswordConfirmVisible ? 'text' : 'password'"
-          :append-inner-icon="isPasswordConfirmVisible ? 'mdi-eye-off' : 'mdi-eye'"
-          @click:append-inner="isPasswordConfirmVisible = !isPasswordConfirmVisible"
-          :rules="[
-            requiredValidator,
-            confirmedValidator(formData.password_confirmation, formData.password)
-          ]"
-        ></v-text-field>
-      </v-col>
-      <v-col cols="12">
-        <v-radio-group v-model="formData.userType" :rules="[requiredValidator]" row>
-          <v-radio label="Teacher" value="teacher"></v-radio>
-          <v-radio label="Admin" value="admin"></v-radio>
-        </v-radio-group>
-      </v-col>
-    </v-row>
+    <v-col cols="12">
+      <v-text-field
+        v-model="formData.password_confirmation"
+        label="Password Confirmation"
+        :type="isPasswordConfirmVisible ? 'text' : 'password'"
+        :append-inner-icon="isPasswordConfirmVisible ? 'mdi-eye-off' : 'mdi-eye'"
+        @click:append-inner="isPasswordConfirmVisible = !isPasswordConfirmVisible"
+        :rules="[
+          requiredValidator,
+          confirmedValidator(formData.password_confirmation, formData.password)
+        ]"
+      ></v-text-field>
+    </v-col>
+    <v-col cols="12">
+      <v-radio-group v-model="formData.userType" :rules="[requiredValidator]" row>
+        <v-radio label="Teacher" value="teacher"></v-radio>
+        <v-radio label="Admin" value="admin"></v-radio>
+      </v-radio-group>
+    </v-col>
+  </v-row>
 
-    <v-btn
-      class="mt-2"
-      type="submit"
-       color="#00A4E5"
-      prepend-icon="mdi-account-plus"
-      :disabled="formAction.formProcess"
-      :loading="formAction.formProcess"
-      block
-    >
-      Register
-    </v-btn>
-  </v-form>
-    </v-card>
+  <v-btn
+    class="mt-2"
+    type="submit"
+     color="#00A4E5"
+    prepend-icon="mdi-account-plus"
+    :disabled="formAction.formProcess"
+    :loading="formAction.formProcess"
+    block
+  >
+    Register
+  </v-btn>
+</v-form>
+  </v-card>
 </template>
 
 <script lang="ts" setup>
@@ -78,10 +78,10 @@ const emit = defineEmits(['registration-success', 'close-dialog']);
 const toast = useToast();
 
 const formData = ref({
-  email: '',
-  password: '',
-  password_confirmation: '',
-  userType: ''
+email: '',
+password: '',
+password_confirmation: '',
+userType: ''
 });
 const formAction = ref({ formProcess: false });
 const isPasswordVisible = ref(false);
@@ -90,42 +90,43 @@ const isPasswordConfirmVisible = ref(false);
 const authUserStore = useAuthUserStore();
 
 async function onFormSubmit() {
-  formAction.value.formProcess = true;
+formAction.value.formProcess = true;
 
-  const { error } = await authUserStore.registerUser(
-    formData.value.email,
-    formData.value.password,
-    formData.value.userType
-  );
+const { error } = await authUserStore.registerUser(
+  formData.value.email,
+  formData.value.password,
+  formData.value.userType
+);
 
-  formAction.value.formProcess = false;
+formAction.value.formProcess = false;
 
-  if (error) {
-    toast.error(`Registration error: ${error.message}`, {
-        //@ts-ignore
-      position: 'top-left',
-      timeout: 3000,
-      closeOnClick: true,
-    });
-  } else {
-    toast.success('Registration successful', {
-        //@ts-ignore
-      position: 'top-left',
-      timeout: 3000,
-      closeOnClick: true,
-    });
- 
-    emit('close-dialog');
-  }
+if (error) {
+  //@ts-ignore
+  toast.error(`Registration error: ${error.message}`, {
+    //@ts-ignore
+    position: 'top-left',
+    timeout: 3000,
+    closeOnClick: true,
+  });
+} else {
+  toast.success('Registration successful', {
+    //@ts-ignore
+    position: 'top-left',
+    timeout: 3000,
+    closeOnClick: true,
+  });
+  emit('registration-success');
+  emit('close-dialog');
+}
 }
 
 </script>
 
 <style scoped>
 .close-icon {
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  cursor: pointer;
+position: absolute;
+top: 10px;
+right: 10px;
+cursor: pointer;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,14 @@ import { registerPlugins } from '@/plugins'
 import "vue-toastification/dist/index.css";
 import VueSidebarMenu from 'vue-sidebar-menu'
 import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+import Toast from "vue-toastification";
 // Components
 import App from './App.vue'
 
 // Composables
 import { createApp } from 'vue'
 
-const app = createApp(App)
+const app = createApp(App).use(Toast);
 
 app.config.warnHandler = () => {};
 
