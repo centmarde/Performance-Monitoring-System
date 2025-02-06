e64cec98b6695f06f530f8698a21d1eb04067e4c
jipenalver <159101935+centmarde@users.noreply.github.com>
2025-02-06 22:27:51 +0800

    feat: added Teachers Account on Admin page

---
+++ src/pages/TeacherAccount.vue
@@ -0,0 +1,23 @@
+<template>
+   <LayoutWrapper>
+      <template #content>
+        <div>
+          <h1 class="text-center">teacher Profile</h1>
+        </div>
+      </template>
+    </LayoutWrapper>
+</template>
+
+<script>
+import LayoutWrapper from "../layouts/LayoutWrapper.vue";
+
+    export default {
+        components: {
+            LayoutWrapper,
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
