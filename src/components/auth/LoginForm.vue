a05bd08c1d6cb8954fe30460c4def51075ec37f0
ccqg <gabaleschrist1@gmail.com>
2025-02-09 19:00:57 +0800

    feat: changes some colors

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -51,7 +51,7 @@
             </v-btn>
           </v-form>
 
-          <h5 class="text-center text-secondary mt-4 mb-3">Or log in using</h5>
+          <h5 class="text-center text-secondary mt-4 mb-3">Login with</h5>
           <div class="social-buttons">
             <v-btn variant="outlined">
               <v-icon color="red">mdi-google</v-icon>
