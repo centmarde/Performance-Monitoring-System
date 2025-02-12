dfbad74bc8751261b448844b893e45f8d6a4416e
ccqg <gabaleschrist1@gmail.com>
2025-02-12 14:57:27 +0800

    fix: center missed activities

--- src/components/system/MissedActivites.vue
+++ src/components/system/MissedActivites.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-row justify="end">
+    <v-row justify="center">
       <v-col cols="auto">
         <v-card class="pa-3 rounded-card glass-card">
           <h4 class="font-weight-bold text-end">
