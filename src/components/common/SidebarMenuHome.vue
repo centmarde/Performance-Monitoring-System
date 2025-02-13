f58f03f8071bd27f1a4bbe9630bd99abe4a1fd8d
ccqg <gabaleschrist1@gmail.com>
2025-02-14 01:11:01 +0800

    feat: put the profile in the bottom

--- src/components/common/SidebarMenuHome.vue
+++ src/components/common/SidebarMenuHome.vue
@@ -73,10 +73,10 @@ onMounted(() => {
 
 // Define the menu with conditional links for Admin and Teachers
 const menu = ref([
-  { title: "Account", icon: "mdi-account", href: "/profiles" },
   { title: "Dashboard", icon: "mdi-view-dashboard", href: "/home" },
   { title: "Data Entry", icon: "mdi-file-document-edit", href: "/data_entry" },
   { title: "Tracking", icon: "mdi-history", href: "/tracking" },
+  { title: "Settings", icon: "mdi-account", href: "/profiles" },
 ]);
 </script>
 
