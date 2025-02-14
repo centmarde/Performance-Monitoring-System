8eb6f6fd223c2c56f587f7c82b75087d348f8a3d
ccqg <gabaleschrist1@gmail.com>
2025-02-14 10:35:07 +0800

    feat: added navbar in index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,77 +1,94 @@
 <template>
-  <v-container>
-    <v-row align="center" justify="center">
-      <v-col cols="12" sm="10">
-        <v-card class="elevation-6 mt-10 rounded-lg">
-          <v-window v-model="step">
-            <!-- Login Section -->
-            <v-window-item :value="1">
-              <v-row>
-                <LoginForm></LoginForm>
-                <v-col cols="12" md="6" class="side-panel rounded-bl-xl">
-                  <div class="panel-content">
-                    <v-card-text class="white--text">
-                      <v-img
-                        class="mx-auto rounded-circle"
-                        src="/images/Logo-school.png"
-                        width="50%"
-                      ></v-img>
-                      <h2 class="text-center mt-3">
-                        Los Angeles National High School
-                      </h2>
-                      <h3 class="text-center mt-10">
-                        Don't Have an Account Yet?
-                      </h3>
-                    </v-card-text>
-                    <div class="text-center">
-                      <v-btn class="action-btn" outlined dark @click="step++">
-                        SIGN UP
-                      </v-btn>
-                    </div>
-                  </div>
-                </v-col>
-              </v-row>
-            </v-window-item>
+  <v-app>
+    <NavBar />
 
-            <!-- Register Section -->
-            <v-window-item :value="2">
-              <v-row>
-                <v-col cols="12" md="6" class="side-panel rounded-br-xl">
-                  <div class="panel-content">
-                    <v-card-text class="white--text">
-                      <v-img
-                        class="mx-auto rounded-circle"
-                        src="/images/Logo-school.png"
-                        width="50%"
-                      ></v-img>
-                      <h2 class="text-center mt-3">
-                        Los Angeles National High School
-                      </h2>
-                      <h3 class="text-center mt-10">Already Signed up?</h3>
-                    </v-card-text>
-                    <div class="text-center">
-                      <v-btn class="action-btn" outlined dark @click="step--">
-                        LOG IN
-                      </v-btn>
-                    </div>
-                  </div>
-                </v-col>
+    <v-main>
+      <v-container>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="10">
+            <v-card class="elevation-6 mt-10 rounded-lg">
+              <v-window v-model="step">
+                <!-- Login Section -->
+                <v-window-item :value="1">
+                  <v-row>
+                    <LoginForm></LoginForm>
+                    <v-col cols="12" md="6" class="side-panel rounded-bl-xl">
+                      <div class="panel-content">
+                        <v-card-text class="white--text">
+                          <v-img
+                            class="mx-auto rounded-circle"
+                            src="/images/Logo-school.png"
+                            width="50%"
+                          ></v-img>
+                          <h2 class="text-center mt-3">
+                            Los Angeles National High School
+                          </h2>
+                          <h3 class="text-center mt-10">
+                            Don't Have an Account Yet?
+                          </h3>
+                        </v-card-text>
+                        <div class="text-center">
+                          <v-btn
+                            class="action-btn"
+                            outlined
+                            dark
+                            @click="step++"
+                          >
+                            SIGN UP
+                          </v-btn>
+                        </div>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
 
-                <v-col cols="12" md="6">
-                  <RegisterForm></RegisterForm>
-                </v-col>
-              </v-row>
-            </v-window-item>
-          </v-window>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+                <!-- Register Section -->
+                <v-window-item :value="2">
+                  <v-row>
+                    <v-col cols="12" md="6" class="side-panel rounded-br-xl">
+                      <div class="panel-content">
+                        <v-card-text class="white--text">
+                          <v-img
+                            class="mx-auto rounded-circle"
+                            src="/images/Logo-school.png"
+                            width="50%"
+                          ></v-img>
+                          <h2 class="text-center mt-3">
+                            Los Angeles National High School
+                          </h2>
+                          <h3 class="text-center mt-10">Already Signed up?</h3>
+                        </v-card-text>
+                        <div class="text-center">
+                          <v-btn
+                            class="action-btn"
+                            outlined
+                            dark
+                            @click="step--"
+                          >
+                            LOG IN
+                          </v-btn>
+                        </div>
+                      </div>
+                    </v-col>
+
+                    <v-col cols="12" md="6">
+                      <RegisterForm></RegisterForm>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts" setup>
 import { ref } from "vue";
 import { useDisplay } from "vuetify";
+import NavBar from "@/components/common/NavBar.vue";
 
 const step = ref(1);
 const { mobile } = useDisplay();
