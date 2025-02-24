c54066884a08ccec1bd85014b2fef91f7f384e2e
ccqg <gabaleschrist1@gmail.com>
2025-02-24 22:28:43 +0800

    fix: remove cms

--- src/pages/home/DataEntry.vue
+++ src/pages/home/DataEntry.vue
@@ -252,7 +252,6 @@
               - {{ activeSubject || "Subject" }}
             </v-card-title>
 
-            <!-- Content -->
             <v-card-text class="mt-4">
               <v-container>
                 <v-row justify="center">
