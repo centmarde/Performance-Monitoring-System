81928e596cdb965af31098a1f4e9ba79aea7ea03
ccqg <gabaleschrist1@gmail.com>
2025-02-26 22:39:19 +0800

    fixed

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+// Router
 import router from "./router";
 
+// Plugins
 import { registerPlugins } from "@/plugins";
 import VueSidebarMenu from "vue-sidebar-menu";
 import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
@@ -18,16 +20,19 @@ import "vue-toastification/dist/index.css";
 import DataTable from "datatables.net-vue3";
 import DataTablesCore from "datatables.net-dt";
 import "@/styles/styles.css";
-
+// Custom Styles
 import "@/styles/logout.css";
 
+// Perfect Scrollbar
 import { PerfectScrollbarPlugin } from "vue3-perfect-scrollbar";
 import "vue3-perfect-scrollbar/style.css";
 
+// Use DataTables
 DataTable.use(DataTablesCore);
 
+// Toast Configuration
 const toastOptions = {
-  toastClassName: "custom-toast",
+  toastClassName: "custom-toast", // Custom styling
   position: "top-right",
   timeout: 3000,
   closeOnClick: true,
@@ -37,13 +42,14 @@ const toastOptions = {
   transition: "Vue-Toastification__fade",
 };
 
+// Create and Mount App
 const app = createApp(App)
   .use(router)
   .use(Toast, toastOptions)
   .use(PerfectScrollbarPlugin)
   .use(VueSidebarMenu);
 
-app.config.warnHandler = () => {};
+app.config.warnHandler = () => {}; // Suppress warnings
 
 registerPlugins(app);
 app.mount("#app");
