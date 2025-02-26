1247d18f5462fc7f03013a4081a4a4e000e0b831
ccqg <gabaleschrist1@gmail.com>
2025-02-26 22:38:46 +0800

    fix: plugins

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ import App from "./App.vue";
 // Router
 import router from "./router";
 
-// Plugins
 import { registerPlugins } from "@/plugins";
 import VueSidebarMenu from "vue-sidebar-menu";
 import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
