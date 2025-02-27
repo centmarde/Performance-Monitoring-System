8bf4751e62548a8e4a126848ef404c0b943034dc
ccqg <gabaleschrist1@gmail.com>
2025-02-27 22:59:19 +0800

    fix plug

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ import App from "./App.vue";
 // Router
 import router from "./router";
 
+// Plugins
 import { registerPlugins } from "@/plugins";
 import VueSidebarMenu from "vue-sidebar-menu";
 import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
