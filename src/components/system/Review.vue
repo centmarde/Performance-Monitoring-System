fd71ddcb6b704464e474314d9b3f477c1fc9e6e0
ccqg <gabaleschrist1@gmail.com>
2025-03-01 16:48:13 +0800

    fix: adjust

--- src/components/system/Review.vue
+++ src/components/system/Review.vue
@@ -202,7 +202,6 @@ export default defineComponent({
           return acc;
         }, {});
 
-        // Convert to array and sort quarters
         availableSubjects.value = Object.values(subjectQuartersMap).map(
           ({ subject, quarters }) => ({
             ...subject,
