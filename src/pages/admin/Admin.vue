0132f6a49968ecf8818c05925ca8e85cfda04b37
ccqg <gabaleschrist1@gmail.com>
2025-03-05 23:55:27 +0800

    fix

--- src/pages/admin/Admin.vue
+++ src/pages/admin/Admin.vue
@@ -323,6 +323,7 @@ const filteredItems = computed(() => {
   });
 });
 
+// Paginated Items (dynamic based on selected itemsPerPage)
 const totalPages = computed(() =>
   Math.ceil(filteredItems.value.length / itemsPerPage.value)
 );
