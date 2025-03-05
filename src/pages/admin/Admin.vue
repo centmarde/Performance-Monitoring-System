e28f71ab02180b6018ac1aef091c69e4ac3cf77a
ccqg <gabaleschrist1@gmail.com>
2025-03-06 00:03:12 +0800

    remove

--- src/pages/admin/Admin.vue
+++ src/pages/admin/Admin.vue
@@ -491,7 +491,7 @@ const selectedTable = ref("users");
   color: white !important;
 }
 .custom-pagination {
-  --v-pagination-active-color: #ffffff !important; /* White text */
+  --v-pagination-active-color: #ffffff !important;
   --v-pagination-active-bg: #004d40 !important; /* Teal background */
   --v-pagination-item-color: #ffffff !important; /* White text for inactive items */
   --v-pagination-item-bg: transparent !important; /* Transparent background */
