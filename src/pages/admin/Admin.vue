8e027310c83f55f941b465a253d0f2059e85c474
ccqg <gabaleschrist1@gmail.com>
2025-03-05 23:55:10 +0800

    fix

--- src/pages/admin/Admin.vue
+++ src/pages/admin/Admin.vue
@@ -344,7 +344,6 @@ const handleItemsPerPageChange = () => {
   currentPage.value = 1;
 };
 
-// Pagination methods
 const nextPage = () => {
   if (currentPage.value < totalPages.value) {
     currentPage.value++;
