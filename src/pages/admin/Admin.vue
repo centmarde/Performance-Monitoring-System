c6d94d454b77ad28426531172801f0e03771f113
ccqg <gabaleschrist1@gmail.com>
2025-03-07 22:11:19 +0800

    clean

--- src/pages/admin/Admin.vue
+++ src/pages/admin/Admin.vue
@@ -108,7 +108,6 @@ const filteredItems = computed(() => {
   });
 });
 
-// Paginated Items (dynamic based on selected itemsPerPage)
 // Calculate total pages
 const totalPages = computed(() =>
   Math.ceil(filteredItems.value.length / itemsPerPage.value)
