436c81dba064ef654ec62fdbbc92a72bab649551
ccqg <gabaleschrist1@gmail.com>
2025-03-08 20:16:52 +0800

    clean

--- src/pages/admin/Admin.vue
+++ src/pages/admin/Admin.vue
@@ -351,7 +351,6 @@ const handleItemsPerPageChange = () => {
   currentPage.value = 1;
 };
 
-// Pagination methods
 const nextPage = () => {
   if (currentPage.value < totalPages.value) {
     currentPage.value++;
