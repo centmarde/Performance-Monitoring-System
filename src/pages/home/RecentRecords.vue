719fbc3d935501c680dd5257016274f67c7b0acf
ccqg <gabaleschrist1@gmail.com>
2025-03-10 14:50:52 +0800

    feat: name colors when failing,passing and average

--- src/pages/home/RecentRecords.vue
+++ src/pages/home/RecentRecords.vue
@@ -794,7 +794,7 @@ const saveChanges = async (item) => {
 };
 const getGradeClass = (grade) => {
   if (grade < 75) return "failing"; // Red
-  if (grade >= 90) return "excellent"; // Green
+  if (grade >= 90) return "excellent";
   if (grade >= 80) return "good";
   if (grade >= 75) return "passing";
   return "";
