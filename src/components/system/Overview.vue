d06726af0e676b01e1c0d1b510e9031874ba349f
ccqg <gabaleschrist1@gmail.com>
2025-03-21 00:54:37 +0800

    fixed

--- src/components/system/Overview.vue
+++ src/components/system/Overview.vue
@@ -160,10 +160,6 @@ export default {
 
     async function fetchFailingStudents() {
       // Get class records created by this teacher
-      const { data: classRecords, error: recordError } = await supabase
-        .from("class_record")
-        .select("id")
-        .eq("teacher_id", userId.value);
 
       if (recordError || !classRecords.length) {
         console.error("Error fetching class records:", recordError);
